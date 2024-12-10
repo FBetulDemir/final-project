3d9cf8c6c0c23096b00694ddfc57dca15ea1f62b
Yui Jensen <kyanyui67@gmail.com>
2024-12-10 10:51:24 +0100

    Added ticket.css and ticket.tsx

--- final-project/client/src/App.tsx
+++ final-project/client/src/App.tsx
@@ -1,9 +1,12 @@
-import "./App.css";
+import Ticket from './components/Ticket/Ticket';
+import './App.css';
 
 function App() {
   return (
     <>
       <h1>It's ShowTime!</h1>
+
+      <Ticket />
     </>
   );
 }
