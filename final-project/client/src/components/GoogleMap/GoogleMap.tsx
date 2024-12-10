2728082f2c069e5516a8bba388f5923ab105220b
Yui Jensen <kyanyui67@gmail.com>
2024-12-10 17:17:15 +0100

    Installed react-google-maps and showed google map

--- final-project/client/src/components/GoogleMap/GoogleMap.tsx
+++ final-project/client/src/components/GoogleMap/GoogleMap.tsx
@@ -1,7 +1,27 @@
-import './GoogleMac.css';
+import './GoogleMap.css';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
 
 const GoogleMap = () => {
-  return <></>;
+  //* For now I just added Malmö latitude and longitude
+  const position = { lat: Number(55.605), lng: Number(13.0038) };
+  return (
+    <>
+      <APIProvider
+        apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}
+        language='en'
+      >
+        <div style={{ height: '50vh', width: '100%' }}>
+          <Map
+            zoom={9}
+            center={position}
+            mapId={import.meta.env.VITE_GOOGLE_MAP_ID}
+          >
+            <AdvancedMarker position={position}></AdvancedMarker>
+          </Map>
+        </div>
+      </APIProvider>
+    </>
+  );
 };
 
 export default GoogleMap;
