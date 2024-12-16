9b81fbcf2177c184180144b145746e408b91c6de
Esmatt123 <105745382+Esmatt123@users.noreply.github.com>
2024-12-17 00:37:06 +0100

    Revert "Dev"

--- final-project/client/src/App.tsx
+++ final-project/client/src/App.tsx
@@ -1,20 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import Register from "./pages/Register.tsx";
-import React from 'react';
-import Browser from "./components/Browser";
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Browser />  {/* Componente Browser visible en todas las páginas */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <>
+      <h1>It's ShowTime!</h1>
+    </>
   );
-};
+}
 
 export default App;
