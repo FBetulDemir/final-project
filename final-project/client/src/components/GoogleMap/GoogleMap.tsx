a40f71b8694ffc08637a4fda2ed5d34dcba0a23f
Yui Jensen <kyanyui67@gmail.com>
2024-12-16 20:51:30 +0100

    Remove Back end

--- final-project/client/src/components/GoogleMap/GoogleMap.tsx
+++ final-project/client/src/components/GoogleMap/GoogleMap.tsx
@@ -1,43 +1,22 @@
 import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
-import { useState, useEffect } from 'react';
 
-interface MapConfig {
-  apiKey: string;
-  mapId: string;
-  position: {
-    lat: number;
-    lng: number;
-  };
-}
-const GoogleMap: React.FC<MapConfig> = () => {
-  const [mapConfig, setConfig] = useState<MapConfig | null>(null);
+const GoogleMap = () => {
+  //* For now I just added Malmö latitude and longitude
+  const position = { lat: Number(55.605), lng: Number(13.0038) };
 
-  useEffect(() => {
-    const fetchConfig = async () => {
-      try {
-        const response = await fetch('http://localhost:5001/api/map');
-        if (!response.ok) {
-          throw new Error('Failed to fetch map ');
-        }
-        const data = await response.json();
-        setConfig(data);
-      } catch (error) {
-        console.error('Error fetching map config', error);
-      }
-    };
-
-    fetchConfig();
-  }, []);
-
-  if (!mapConfig) {
-    return <p>Loading..</p>;
-  }
   return (
     <>
-      <APIProvider apiKey={mapConfig.apiKey} language='en'>
+      <APIProvider
+        apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}
+        language='en'
+      >
         <div style={{ height: '50vh', width: '100%' }}>
-          <Map zoom={10} center={mapConfig.position} mapId={mapConfig.mapId}>
-            <AdvancedMarker position={mapConfig.position}></AdvancedMarker>
+          <Map
+            zoom={8}
+            center={position}
+            mapId={import.meta.env.VITE_GOOGLE_MAP_ID}
+          >
+            <AdvancedMarker position={position}></AdvancedMarker>
           </Map>
         </div>
       </APIProvider>
@@ -46,30 +25,3 @@ const GoogleMap: React.FC<MapConfig> = () => {
 };
 
 export default GoogleMap;
-// import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
-
-// const GoogleMap = () => {
-//   //* For now I just added Malmö latitude and longitude
-//   const position = { lat: Number(55.605), lng: Number(13.0038) };
-
-//   return (
-//     <>
-//       <APIProvider
-//         apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}
-//         language='en'
-//       >
-//         <div style={{ height: '50vh', width: '100%' }}>
-//           <Map
-//             zoom={10}
-//             center={position}
-//             mapId={import.meta.env.VITE_GOOGLE_MAP_ID}
-//           >
-//             <AdvancedMarker position={position}></AdvancedMarker>
-//           </Map>
-//         </div>
-//       </APIProvider>
-//     </>
-//   );
-// };
-
-// export default GoogleMap;
